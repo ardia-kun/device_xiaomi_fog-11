6cf27838fe01c3d6c4f1cf47bd5744d94c091737
Ardia-kun <85530328+ardia-kun@users.noreply.github.com>
2025-01-19 14:03:05 +0000

    fog: @AospExtended bringups.
    Signed-off-by: Ardia-kun <85530328+ardia-kun@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_fog.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_fog-user \
-    lineage_fog-userdebug \
-    lineage_fog-eng
+    $(LOCAL_DIR)/aosp_fog.mk
